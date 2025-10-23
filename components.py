820cffa89fafc34040785de77e04ced71db8be05
Sam Sepiol <jakubsopel7@gmail.com>
2025-10-23 17:41:26 +0200

    refacotr, play on callback, component for dropdown

--- components.py
+++ components.py
@@ -1,14 +1,36 @@
+import discord
 from discord.ui import Select
+from discord import VoiceProtocol
 from discord.components import SelectOption
+from schemas import TrackSchema
+from utils import play_file
 
 
-def create_music_select(list_of_items: list[str]) -> Select:
-    return Select(
-        placeholder="Select music",
-        max_values=5,
-        options=[
-            SelectOption(label="siema", value="test"),
-            SelectOption(label="siema", value="test"),
-            SelectOption(label="siema", value="test"),
-        ],
-    )
+class SelectMusic(Select):
+    def __init__(self, tracks: list[TrackSchema], voice_client: VoiceProtocol):
+        options = [
+            SelectOption(label=song["title"], value=song["url"]) for song in tracks
+        ]
+        self.voice_client = voice_client
+        super().__init__(
+            placeholder="Pick your poison",
+            options=options,
+            min_values=1,
+            max_values=1,
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        track_url: str = self.values[0]  # 0 is the value picked by the user
+        await interaction.response.send_message(
+            f"You chose **{self.values[0]}**!", ephemeral=True
+        )
+        if track_url and not self.voice_client.is_playing():
+            await play_file(
+                track_url=track_url,
+                interaction=interaction,
+                voice_client=self.voice_client,
+            )
+        else:
+            await interaction.followup.send(
+                "No valid URL or already playing.", ephemeral=True
+            )
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from discord.ui.view import View
+from components import SelectMusic
 import os
 import yt_dlp
 import asyncio
 import ffmpeg
 from collections import deque
+from schemas import TrackSchema
 
 from typing import Any
 
@@ -50,7 +53,7 @@ async def play_music(interaction: discord.Interaction, song_query: str):
     if voice_channel is None:
         msg = await interaction.followup.send("You must be in a voice channel")
         await cleanup(msg)
-        return
+        return None
 
     voice_client = interaction.guild.voice_client
 
@@ -83,32 +86,15 @@ async def play_music(interaction: discord.Interaction, song_query: str):
         return
 
     first_five_tracks: list[dict[str, Any]] = tracks[:5]
-    first_five_titles: list[str] = [track["title"] for track in first_five_tracks]
+    first_five_titles: list[TrackSchema] = [
+        {"title": track["title"], "url": track["url"]} for track in first_five_tracks
+    ]
 
-    print(first_five_titles)
-
-    first_track = tracks[0]
-
-    audio_url = first_track["url"]
-    title = first_track.get("title", "Untitled")
-
-    guild_id = str(interaction.guild_id)
-    if SONG_QUEUES.get(guild_id) is None:
-        SONG_QUEUES[guild_id] = deque()
-
-    SONG_QUEUES[guild_id].append((audio_url, title))
-
-    if voice_client.is_playing() or voice_client.is_paused():
-        msg = await interaction.followup.send(f"Added to queue: **{title}**")
-    else:
-        msg = await interaction.followup.send(f"Now playing: **{title}**")
-        await play_next_song(voice_client, guild_id, interaction.channel)
-
-    await send_to_archive(
-        f"Added to queue: **{title}** requested by {interaction.user.name}",
-        1427664996497621095,
+    tracks_view = View()
+    tracks_view.add_item(
+        SelectMusic(tracks=first_five_titles, voice_client=voice_client)
     )
-    await cleanup(msg)
+    await interaction.followup.send("Pick one", view=tracks_view, ephemeral=True)
 
 
 @bot.tree.command(name="stop", description="stop song nigga")
@@ -161,25 +147,9 @@ async def play_next_song(voice_client, guild_id, channel):
         SONG_QUEUES[guild_id] = deque()
 
 
-async def send_to_archive(message, CHANNEL_ID):
-    channel = bot.get_channel(CHANNEL_ID)
-    if channel:
-        await channel.send(message)
-
-
 async def cleanup(message, timer=60):
     await asyncio.sleep(timer)
     await message.delete()
-
-
-async def dropdownMenu():
-    ydl_options = {
-        "quiet": True,
-        "extract_flat": True,
-        "skip_download": True,
-        "noplaylist": True,
-    }
-    pass
 
 
 bot.run(token)
